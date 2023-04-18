3472f54a9999bf734e47fd70dafa97b6e718506a
Alisson Nascimento <alissonnascimento321@gmail.com>
2023-04-18 11:35:24 -0300

    add router

--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -1,60 +1,46 @@
 import { ChatGPT } from "./Chatbox";
 import style from "../Home.module.css";
 import React, { useState } from "react";
-import {
-  BsSlack,
-  BsJoystick,
-} from "react-icons/bs";
-
+import { BsSlack, BsJoystick } from "react-icons/bs";
 
 import { Link } from "react-router-dom";
 import { Navbar } from "./Navbar";
 
-
 export function Home() {
-
-
   return (
     <div className={style.homecontainer}>
-     
-
       <div>
-      < Navbar />
+        <Navbar />
 
-          <div>
-            <div className={`${style.inicar} ${style.link}`}>
-              <span className={style.start}>
-                <strong>Guia</strong>
-              </span>
-              <span className={style.tutor}>
-                <strong>do</strong>
-              </span>
-              <span className={style.personalizado}>
-                <strong>Saber</strong>
-              </span>
-              <h2 className={style.subtitle}>
-                <strong>Aprenda com praticidade!</strong>
-              </h2>
-              <li className={style.journey} >
-                
-                <Link to="Chat"> <BsSlack /> &nbsp; Chat de aprendizagem</Link>
-            
-              </li>
-            </div>
+        <div>
+          <div className={`${style.inicar} ${style.link}`}>
+            <span className={style.start}>
+              <strong>Guia</strong>
+            </span>
+            <span className={style.tutor}>
+              <strong>do</strong>
+            </span>
+            <span className={style.personalizado}>
+              <strong>Saber</strong>
+            </span>
+            <h2 className={style.subtitle}>
+              <strong>Aprenda com praticidade!</strong>
+            </h2>
+
+            <Link to="Chat" className={style.journey}>
+              {" "}
+              <BsSlack /> &nbsp; Chat de aprendizagem
+            </Link>
           </div>
-        
+        </div>
 
-      
-          <div className={`${style.inicar} ${style.quiz}`}>
-            <button className={style.buttonquiz} >
-
-            <Link to="Quiz"> <BsJoystick /> &nbsp; Teste Seu Conhecimento</Link>  
-
-           </button>
-          </div>
-       
-
-     
+        <div className={`${style.inicar} ${style.quiz}`}>
+            <Link to="Quiz" className={style.buttonquiz}>
+              {" "}
+              <BsJoystick /> &nbsp; Teste Seu Conhecimento
+            </Link>
+          
+        </div>
       </div>
     </div>
   );
